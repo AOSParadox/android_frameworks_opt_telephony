0e63e194ade8073b68c9f2344ad2e09cec2dfb3b
Steve Kondik <steve@cyngn.com>
2016-11-06 01:49:51 -0800

    telephony: Fix NPE when switching data SIM
    Change-Id: If30a7d33af56a8d66eb36d516aef6ebe165be83a
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- src/java/com/android/internal/telephony/Phone.java
+++ src/java/com/android/internal/telephony/Phone.java
@@ -3355,7 +3355,9 @@ public abstract class Phone extends Handler implements PhoneInternalInterface {
     }
 
     public void unregisterForDataEnabledChanged(Handler h) {
-        mDcTracker.unregisterForDataEnabledChanged(h);
+        if (mDcTracker != null) {
+            mDcTracker.unregisterForDataEnabledChanged(h);
+        }
     }
 
     public IccSmsInterfaceManager getIccSmsInterfaceManager(){
